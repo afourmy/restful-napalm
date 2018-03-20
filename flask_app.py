fee898f75f66c44e2257d232c99010f0b6877afb
minto <minto@hotmail.fr>
2018-03-20 07:51:32 +0100

    restful napalm

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_restful import Resource, Api
+from napalm import get_network_driver
 from os.path import abspath, dirname
 from sys import path
 
@@ -8,10 +10,27 @@ if path_app not in path:
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
+api = Api(app)
+username = 'cisco'
+password = 'cisco'
 
-@app.route('/')
-def index():
-    return 'ok'
+
+class RestfulNapalm(Resource):
+    def get(self, ip_address, os, getter):
+        try:
+            driver = get_network_driver(os)
+            with driver(ip_address, username, password) as device:
+                device.open()
+                getter = getattr(device, getter)
+            return getter
+        except Exception as e:
+            return str(e)
+
+
+api.add_resource(
+    RestfulNapalm,
+    '/<string:ip_address>/<string:os>/<string:getter>'
+)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
